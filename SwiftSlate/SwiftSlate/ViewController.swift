a1c652d0124c969f687d4e98962bb389e8b17728
Kyle Oba <koba@mudphone.com>
2014-08-05 15:49:53 -1000

    Add basic Swift iOS project.

---
+++ SwiftSlate/SwiftSlate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSlate
+//
+//  Created by Kyle Oba on 8/5/14.
+//  Copyright (c) 2014 Pas de Chocolat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
